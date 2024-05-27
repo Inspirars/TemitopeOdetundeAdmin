88afb8083da29fcad5307973750cbc5730b8c908
oladee <deemajor230600@gmail.com>
2024-05-27 19:05:09 +0100

    solved tinymyce issues

--- public/javascripts/my-tinymce-config.js
+++ public/javascripts/my-tinymce-config.js
@@ -1,13 +1,11 @@
 tinymce.init({
   selector: 'textarea',
   resize : "both",
-  plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-  toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-  tinycomments_mode: 'embedded',
-  tinycomments_author: 'Temitope Odetunde',
-  mergetags_list: [
-    { value: 'TemiTope', title: 'TemiTope' },
-    { value: 'southwarkrehab@gmail.com', title: 'southwarkrehab@gmail.com' },
+  plugins: ['link','image','charmap','preview','anchor','searchreplace','visualblocks','fullscreen','insertdatetime','media','table','help','wordcount'
+
   ],
-  ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+  toolbar: 'undo redo | casechange blocks | bold italic backcolor | ' +
+    'alignleft aligncenter alignright alignjustify | ' +
+    'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
+  
 });
