13e3751522cb487e3eb8d1643a00c32ade24b3b2
oladee <deemajor230600@gmail.com>
2025-06-02 10:19:39 +0100

    doney

--- routes/form.js
+++ routes/form.js
@@ -10,39 +10,39 @@ const User = require('../models/user');
 /* GET home page. */
 let error = ""
 
-router.get('/signup',function(req, res, next) {
-  error = []
-  res.render('signUp', {title: 'Sign Up', error});
-});
-router.post('/signup', asyncHandler(async(req,res,next)=>{
-  let {username, password} = req.body
-  if(!username || !password){
-    error = [{msg :"Missing parameters"}]
-    res.render('signUp', {title : "Sign Up Error", error : error})
-  }
-  username = username.toLowerCase()
-  const existinguser = await User.findOne({username : username}).exec()
-  if (existinguser){
-    error = [{msg :"User already exists"}]
-    res.render('signUp', {title : "Sign Up Error", error : error})
-  }else{
-  var saltHash = genPassword(password)
-  // genpassword returns a salt and hash
+// router.get('/signup',function(req, res, next) {
+//   error = []
+//   res.render('signUp', {title: 'Sign Up', error});
+// });
+// router.post('/signup', asyncHandler(async(req,res,next)=>{
+//   let {username, password} = req.body
+//   if(!username || !password){
+//     error = [{msg :"Missing parameters"}]
+//     res.render('signUp', {title : "Sign Up Error", error : error})
+//   }
+//   username = username.toLowerCase()
+//   const existinguser = await User.findOne({username : username}).exec()
+//   if (existinguser){
+//     error = [{msg :"User already exists"}]
+//     res.render('signUp', {title : "Sign Up Error", error : error})
+//   }else{
+//   var saltHash = genPassword(password)
+//   // genpassword returns a salt and hash
 
-  salt = saltHash.salt
-  hash = saltHash.hash
+//   salt = saltHash.salt
+//   hash = saltHash.hash
 
-  const user = new User({
-    username,
-    salt,
-    hash
-  })
+//   const user = new User({
+//     username,
+//     salt,
+//     hash
+//   })
 
-  await user.save()
-  .then(()=>{console.log(user)})
-  res.redirect("/signin")
-  }
-}))
+//   await user.save()
+//   .then(()=>{console.log(user)})
+//   res.redirect("/signin")
+//   }
+// }))
 
 router.get('/signin',(req,res,next)=> {
   res.render('signIn', {title : "Sign In", error : []})
